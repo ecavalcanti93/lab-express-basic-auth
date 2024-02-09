db187e575a9838ab1420c67ae98f88c32e4d9f1e
ecavalcanti683 <ecavalcanti.dg@outlook.com>
2024-02-09 01:21:25 +0100

    Done (123)

---
+++ routes/auth.routes.js
@@ -0,0 +1,114 @@
+const { Router } = require("express");
+const User = require("../models/User.model");
+const mongoose = require("mongoose");
+const router = new Router();
+
+const bcryptjs = require("bcryptjs");
+
+const saltRounds = 10;
+
+router.get("/signup", (req, res) => res.render("auth/signup"));
+router.get("/userProfile", (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+router.post("/signup", (req, res, next) => {
+  // console.log("The form data: ", req.body);
+
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.render("auth/signup", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
+    });
+
+    return;
+  }
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.create({
+        // username: username
+        username,
+        // passwordHash => this is the key from the User model
+        //     ^
+        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+        passwordHash: hashedPassword,
+      });
+    })
+    .then((userFromDB) => {
+      //console.log('Newly created user is: ', userFromDB);
+      res.redirect("/userProfile");
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        console.log(
+          " Username need to be unique. Either username is already used. "
+        );
+
+        res.status(500).render("auth/signup", {
+          errorMessage: "User not found and/or incorrect password.",
+        });
+      } else {
+        next(error);
+      }
+    });
+});
+
+//!LOGIN
+
+router.get("/login", (req, res) => res.render("auth/login"));
+
+router.post("/login", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        console.log("Username not registered.");
+        res.render("auth/login", {
+          errorMessage: "User not found and/or incorrect password.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        //res.render('users/user-profile', { user });
+        req.session.currentUser = user;
+        res.redirect("/userProfile");
+      } else {
+        console.log("Incorrect password.");
+        res.render("auth/login", {
+          errorMessage: "User not found and/or incorrect password.",
+        });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+//!LOGOUT
+
+router.post('/logout', (req, res, next) => {
+
+    req.session.destroy(err => {
+  
+      if (err) next(err);
+  
+      res.redirect('/');
+  
+    });
+  
+  });
+
+module.exports = router;
